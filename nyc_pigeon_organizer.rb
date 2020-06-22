08842dda05226f3307e0d61a456f4abae70f73e7
Christina Sohn <chsohn15@gmail.com>
2020-06-22 02:57:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+<<<<<<< HEAD
   final_results = data.each_with_object({}) do |(key,value), final_array|
     # binding.pry
     value.each do |inner_key, names|
@@ -19,5 +20,17 @@ def nyc_pigeon_organizer(data)
   end 
   #binding.pry
   final_results
+=======
+  data.each_with_object({}) do |(key,value), final_array|
+    # binding.pry
+    value.each do |inner_key, names|
+      names.each do |name|
+        binding.pry
+      end
+    end
+    final_array
+  end |
+  binding.pry
+>>>>>>> e311140e1d9673d7f49a5e38158ecaf3463cce2f
 end
  
